16d5e342a336451495f9ad45c6eea8a1168dc95c
Rogério Rocha <neorogerio@gmail.com>
2021-11-03 21:31:46 -0300

    Resolução do Desafio

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,57 @@
-export function Content() {
+import { useEffect, useState } from "react";
+
+import { MovieCard } from '../components/MovieCard';
+import { Header } from "./Header";
+
+import { api } from '../services/api';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface SelectedGenderIdProps {
+  selectedGenreId: number,
+}
+
+export function Content(props: SelectedGenderIdProps) {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${props.selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+
+    api.get<GenreResponseProps>(`genres/${props.selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [props.selectedGenreId]);
+
   // Complete aqui
+  return (
+    <div className="container">
+      <Header title={selectedGenre.title} />
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+interface GenreResponseProps {
+    title: string;
+}
+
+export function Header(props: GenreResponseProps) {
+    return (
+        <header>
+            <span className="category">Categoria:<span> {props.title}</span></span>
+        </header>
+    )
+
+}
